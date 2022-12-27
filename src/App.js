00e50d23de0fded57867d4704e980145c058d795
davidbiton33 <davidbiton2@gmail.com>
2022-12-27 14:21:00 +0300

    start

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+function App() {
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = await axios(
+        'https://backback-q36f.onrender.com/products/all'
+      );
+      setData(result.data.products);
+    };
+
+    fetchData();
+  }, []);
+
+  return (
+    <div>
+      {data ? (
+        <ul className="products-list">
+          {data.map(product => (
+            <li key={product._id}>{product.name} ({product.price})</li>
+          ))}
+        </ul>
+      ) : (
+        <div>Loading...</div>
+      )}
+    </div>
+  );
+}
+
+export default App;
